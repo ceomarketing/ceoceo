2cb9f1564a6af656eabb31bde43a5d56b28059a4
Lil-Sun <lily.in.the.sun.yc@gmail.com>
2019-04-16 17:59:29 -0700

    Some carousel work

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,47 @@
 import React, { Component } from "react";
+import Carousel from 'react-bootstrap/Carousel';
  
 class Home extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleSelect = this.handleSelect.bind(this);
+
+    this.state = {
+      carouselIndex: 0,
+      direction: null,
+    };
+  }
+
+  handleSelect(selectedIndex, e) {
+    this.setState({
+      carouselIndex: selectedIndex,
+      direction: e.direction,
+    });
+  }
+
   render() {
+    const { index, direction } = this.state;
+
     return (
       <div>
-        <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
- 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+        <Carousel
+          activeIndex={index}
+          direction={direction}
+          onSelect={this.handleSelect}
+        >
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/public/images/placeholders/home-1.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>We empower and connect entrepreneurs in China and Silicon Valley.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </div>
     );
   }
