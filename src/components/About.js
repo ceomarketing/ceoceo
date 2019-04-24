56235f28b738ccaf9e272be27c8555da76b7e79b
yokostan <39684074+yokostan@users.noreply.github.com>
2019-04-23 18:12:24 -0700

    update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,100 @@
 import React, { Component } from "react";
+import '../css/About.css';
  
 class About extends Component {
   render() {
     return (
-      <div>
-        <h2>ABOUT CEO</h2>
-        <h5>To build the best educational and communicational platform of entrepreneurship for all Chinese entrepreneurs and investors around the globe.</h5>
-        <p>Mauris sem velit, vehicula eget sodales vitae,
-        rhoncus eget sapien:</p>
-        <ol>
-          <li>Nulla pulvinar diam</li>
-          <li>Facilisis bibendum</li>
-          <li>Vestibulum vulputate</li>
-          <li>Eget erat</li>
-          <li>Id porttitor</li>
-        </ol>
+      <div class="container-sm">
+        <div class="page-intro">
+          <div class="container">
+          <h1 class="page-title">华人创业者协会</h1>
+          <h1 class="page-title">Chinese Entrepreneur Organziation</h1>
+          <p class="lead">
+           CEO organizes in-depth small-group entrepreneurial talks, brainstorm sessions, etc.
+          </p>
+          </div>  
+        </div>
+
+        <div class="page-content">
+          {/* <div class="section highlight">
+            <div class="container">
+              <div class="row about-organisers">
+                <div class="col-sm-3">
+                  <div class="organiser">
+                    <div class="organiser_header">
+                      <div class="img-wrapper">
+                        <img src="" alt="CEO leaders - XXX" width="80" height="80" class="img-circle"></img>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="organiser">
+                    <div class="organiser_header">
+                      <div class="img-wrapper">
+                        <img src="" alt="CEO leaders - XXX" width="80" height="80" class="img-circle"></img>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="organiser">
+                    <div class="organiser_header">
+                      <div class="img-wrapper">
+                        <img src="" alt="CEO leaders - XXX" width="80" height="80" class="img-circle"></img>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> */}
+          
+          <div class="section">
+            <div class="container-sm">
+              <h2>ABOUT CEO</h2>
+              <h5 id='p1'>To build the best educational and communicational platform of entrepreneurship for all Chinese entrepreneurs and investors around the globe.</h5>
+              <p>
+                CEO (Chinese Entrepreneur Organization, www.ceoceo.org) 华人者创业协会由斯坦福大学学生和校友于2008年在美国硅谷创办，旨在沟通工程师及商务人士，连接正在和准备创业的华人。CEO成员先后创立了聚美优品 (陈欧)、出门问问 (朱祎舟)、Hover Cam (王孟秋）、Alohar Mobile (王辰宇)、小红书 (毛文超)、丹华资本（谷安佳）等优秀企业或创投基金；也有部分成员加入金沙江创投、 IDG、 FreeS、真格基金等知名投资机构；或者进入Google, Twitter, Coursera等硅谷大型科技企业或新兴科技公司。自成立以来，CEO多次举办创业投资讲座，线下创业者聚会等活动，讨论科技趋势和创业想法。也曾邀请雷军、周鸿祎等百余名成功华人创业家与大家面对面分享经验。今年，CEO更是联合硅谷知名投资机构，举办专业技术的闭门讨论，针对最尖端科技的商业化进行头脑风暴。为促进国内外华人创业者之间的交流，CEO Startup Trek每年夏天带领斯坦福校友以及硅谷杰出人才与创业者回国，就新兴技术和商业动向与国内创业者进行交流学习。如今，CEO已有超过4000名会员，其中不仅凝聚了斯坦福大学的学生和校友，还吸引了硅谷及全球的华人创业者的加入。
+              </p>
+              <p>
+                Chinese Entrepreneurs Organization 华人创业者协会（CEO) 是一个正式注册的非营利组织。CEO与斯坦福大学之间不存在隶属关系，不是斯坦福大学的附属单位或延伸机构。
+              </p>
+              <p>
+                CEO - Chinese Entrepreneur Organization (www.ceoceo.org) was founded in 2008 by a group of business-minded students and talented Engineering students at Stanford. The organization serves as a platform to connect Chinese entrepreneurs and people who are interested in entrepreneurship in Silicon Valley. Since our inception, we have been hosting in-depth talks by top investors and outstanding entrepreneurs, facilitating exchange of brilliant ideas among the rich talent pool in our membership base. Many of our members have successfully formed new ventures including 聚美优品 (Ou Chen), 出门问问 (Yizhou Zhu), Hover Cam (Mengqiu Wang), Alohar Mobile (Larry Wang), Xiaohongshu (Charlwin Mao), Danhua Capital(Andrew Gu), or joined top tech companies and VCs, such as. GSR VC, IDG, FreeS, and Google. CEO has also organized summer trip - CEO Startup Trek, to visit outstanding startup companies in China. CEO Startup Trek aims to help Chinese entrepreneurs or talented Engineers in Silicon Valley communicate in person with successful Chinese entrepreneurs and learn from their experience. CEO is dedicated to creating a supportive community for Chinese entrepreneurs, and building the best educational and communicational platform of entrepreneurship for Chinese entrepreneurs and investors around the globe. Currently, CEO has more than 4000 subscribers, including current Stanford students, alumni and young professionals in the Valley.
+              </p>
+              <p>
+                Chinese Entrepreneurs Organization is not endorsed by, involved with or otherwise affiliated with Stanford University. Chinese Entrepreneurs Organization is an independent, non-profit organization that is unrelated to the activities or mission of Stanford University
+              </p>
+              <hr class="mv-3"></hr>
+            </div>
+          </div>  
+
+          <div class="section">
+            <div class="container-sm">
+              <h2>Our Sponsors</h2>
+              <div class="row">
+                <div class="img-wrapper">
+                  <img src="" alt="ZhenFund" width="90%"></img>
+                </div>
+              </div>
+              <div class="row"></div>
+              <div class="row">
+                <div class="img-wrapper">
+                  <img src="" alt="Fenwick" width="90%"></img>
+                </div>
+              </div>
+              <div class="row"></div>
+              <div class="row">
+                <div class="img-wrapper">
+                  <img src="" alt="GSR" width="90%"></img>
+                </div>
+              </div>
+              <div class="row"></div>
+            </div>
+          </div> 
+        </div>
+     
       </div>
     );
   }
